43b2c6b37e1c39da62a5ca97878dcbc459527ebc
U-ICIMSNET\ngattu <ngattu@T45-13844.icims.net>
2017-06-08 08:56:02 -0400

    Adding working input placement/button click

--- loginTest.js
+++ loginTest.js
@@ -1,14 +1,24 @@
 var casper = require('casper').create();
 var links;
 
-var x = require('casper').selectXpath;
+var x = require('casper').selectXPath;
 casper.userAgent('Mozilla/4.0 (comptible; MSIE 6.0; Windows NT 5.1)');
 
 casper.start('http://www.collectsoftware.org/contact');
 
-casper.then(function ()){
+casper.then(function (){
 	this.sendKeys('#Email', "ngattuso@test.com");
+	
+	this.sendKeys('#EmailName', "ngattuso");
 
+	this.sendKeys('#message' , "This is my test message");
+	
 });
+
+casper.thenClick(x('//*[@id="content"]/form/button'), function(){
+	casper.capture("the_testimage.png");
+	console.log("done");
+	});
+
 
 casper.run();
